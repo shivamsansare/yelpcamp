5405b778361977c218116fc5f01fd4004178cf5f
Shivam Sansare <shivam.sansare99@gmail.com>
2020-05-22 12:20:30 +0530

    mongoose

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var authRoutes=require("./routes/index");
 var campgroundRoutes=require("./routes/campground");
 var commentRoutes=require("./routes/comments");
 
-mongoose.connect("mongodb+srv://shivam:yelpcamp@cluster0-jui1f.mongodb.net/yelpcamp?retryWrites=true&w=majority",{useNewUrlParser: true});
+//mongoose.connect("mongodb+srv://shivam:yelpcamp@cluster0-jui1f.mongodb.net/yelpcamp?retryWrites=true&w=majority",{useNewUrlParser: true});
+mongoose.connect("<add database here>");
 
 app.use(require("express-session")({
     secret:"Shivam's created Yelpcamp",
